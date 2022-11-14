15d9a8929983b7781ce5b0be9bef74098faddd06
Nam G VU <namgivu@gmail.com>
2022-11-14 14:33:08 +0700

    simpler code + semicolon

--- components/protocols/polygon/challenges/balance.ts
+++ components/protocols/polygon/challenges/balance.ts
@@ -6,19 +6,19 @@ declare let window: {
 
 const getBalance = async (address: string) => {
   try {
-    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const provider = new ethers.providers.Web3Provider(window.ethereum)
 
     const balance = undefined;
     if (!balance) {
-      throw new Error('Please complete the code');
+      throw new Error('Please complete the code')
     }
+
     return {
       balance: balance.toString(),
-    };
+    }
+
   } catch (error) {
-    return {
-      error: error.message,
-    };
+    return {error: error.message}
   }
 };
 
