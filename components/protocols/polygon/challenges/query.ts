1e86c95d5a94143f9e3066458d0d695ba57f0423
Nam G VU <namgivu@gmail.com>
2022-11-14 13:25:35 +0700

    clearer code
    clearer code
    
    clearer code

--- components/protocols/polygon/challenges/query.ts
+++ components/protocols/polygon/challenges/query.ts
@@ -9,21 +9,16 @@ const query = async () => {
     const provider = new ethers.providers.Web3Provider(window.ethereum)
 
     // get :network :chainId
-    const networkName = await provider.getNetwork().then(
-      (r_network) => {
-        return r_network.name
-      })
-    const chainId = provider.network.chainId
+    const networkName = await provider.getNetwork().then((r_network) => {r_network.name})
+    const chainId     = provider.network.chainId
 
     // get latest block :blockInfo at :blockHeight=latest
     const blockHeight = await provider.getBlockNumber()
     const blockInfo   = await provider.getBlockWithTransactions(blockHeight)
 
     // get :gasPriceAsGwei
-    const gasPriceAsGwei = await provider.getGasPrice().then(
-        (r_gasPrice) => {
-          return ethers.utils.formatUnits(r_gasPrice, 'gwei')
-        })
+    const gasPrice       = await provider.getGasPrice()
+    const gasPriceAsGwei = ethers.utils.formatUnits(gasPrice, 'gwei')
 
     if (!chainId || !blockHeight || !gasPriceAsGwei || !blockInfo) {
       throw new Error('Please complete the code');
