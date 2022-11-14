5ef4850d9028560eee5173749265885399604d32
Nam G VU <namgivu@gmail.com>
2022-11-14 13:11:10 +0700

    clearer code 3rd

--- components/protocols/polygon/challenges/query.ts
+++ components/protocols/polygon/challenges/query.ts
@@ -9,19 +9,21 @@ const query = async () => {
     const provider = new ethers.providers.Web3Provider(window.ethereum)
 
     // get :network :chainId
-    const networkName = await provider.getNetwork().then((r_network) => {
-      return r_network.name;
-    });
-    const chainId = provider.network.chainId;
+    const networkName = await provider.getNetwork().then(
+      (r_network) => {
+        return r_network.name
+      })
+    const chainId = provider.network.chainId
 
     // get latest block :blockInfo at :blockHeight=latest
     const blockHeight = await provider.getBlockNumber()
     const blockInfo   = await provider.getBlockWithTransactions(blockHeight)
 
     // get :gasPriceAsGwei
-    const gasPriceAsGwei = await provider.getGasPrice().then((r_gasPrice) => {
-      return ethers.utils.formatUnits(r_gasPrice, 'gwei');
-    });
+    const gasPriceAsGwei = await provider.getGasPrice().then(
+        (r_gasPrice) => {
+          return ethers.utils.formatUnits(r_gasPrice, 'gwei')
+        })
 
     if (!chainId || !blockHeight || !gasPriceAsGwei || !blockInfo) {
       throw new Error('Please complete the code');
