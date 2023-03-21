14705bea59e243d1b2253e1539548491cad36806
Nam G VU <namgivu@gmail.com>
2023-03-21 13:58:11 +0700

    getGasPrice()  //TODO what unit is this price in?

--- components/protocols/polygon/challenges/query.ts
+++ components/protocols/polygon/challenges/query.ts
@@ -2,26 +2,28 @@ import {ethers} from 'ethers';
 
 declare let window: {
   ethereum: ethers.providers.ExternalProvider;
-}
+};
 
 const query = async () => {
   try {
-    const provider = new ethers.providers.Web3Provider(window.ethereum)
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
 
     // get :network :chainId
-    const networkName = await provider.getNetwork().then((r_network) => {return r_network.name})
-    const chainId     = await provider.network.chainId
+    const networkName = await provider.getNetwork().then((r_network) => {
+      return r_network.name;
+    });
+    const chainId = await provider.network.chainId;
 
     // get latest block :blockInfo at :blockHeight=latest
-    const blockHeight = await provider.getBlockNumber()
-    const blockInfo   = await provider.getBlockWithTransactions(blockHeight)
+    const blockHeight = await provider.getBlockNumber();
+    const blockInfo = await provider.getBlockWithTransactions(blockHeight);
 
     // get :gasPriceAsGwei
-    const gasPrice       = await provider.getGasPrice()
-    const gasPriceAsGwei = await ethers.utils.formatUnits(gasPrice, 'gwei')
+    const gasPrice = await provider.getGasPrice(); //TODO what unit is this price in?
+    const gasPriceAsGwei = await ethers.utils.formatUnits(gasPrice, 'gwei');
 
     if (!chainId || !blockHeight || !gasPriceAsGwei || !blockInfo) {
-      throw new Error('Please complete the code')
+      throw new Error('Please complete the code');
     }
 
     return {
@@ -32,10 +34,9 @@ const query = async () => {
         gasPriceAsGwei,
         blockInfo,
       },
-    }
-
+    };
   } catch (error) {
-    return {error: error.message}
+    return {error: error.message};
   }
 };
 
